93f8255a199bb159847e61019461aee993a5dd7f
mayub42 <md.ahsanayub@outlook.com>
2019-10-31 17:48:44 -0500

    Minor changes

--- Matplotlib_Python/confusion_matrix_generator.py
+++ Matplotlib_Python/confusion_matrix_generator.py
@@ -1,12 +1,39 @@
-#importing the library
 import numpy as np
-import matplotlib.pyplot as plt
 
 def plot_confusion_matrix(cm,
                           target_names,
-                          title="Confusion Matrix",
+                          title='Confusion matrix',
                           cmap=None,
                           normalize=True):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
     import itertools
 
     accuracy = np.trace(cm) / float(np.sum(cm))
@@ -16,16 +43,14 @@ def plot_confusion_matrix(cm,
         cmap = plt.get_cmap('Greys')
 
     plt.figure(figsize=(10, 8))
-    #plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.imshow(cm, cmap=cmap)
-    plt.title(title, fontsize=22, fontweight='bold')
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title,fontsize=24, weight='bold')
     #plt.colorbar()
 
     if target_names is not None:
         tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45, fontsize=22)
-        plt.yticks(tick_marks, target_names, fontsize=20)
-        plt.ylabel("Actual Class",fontsize=20)
+        plt.xticks(tick_marks, target_names, rotation=0,fontsize=22)
+        plt.yticks(tick_marks, target_names, rotation=90, fontsize=22)
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
@@ -35,24 +60,22 @@ def plot_confusion_matrix(cm,
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         if normalize:
             plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     fontsize=26,
-                     weight='bold',
+                     horizontalalignment="center",fontsize=26, weight='bold',
                      color="white" if cm[i, j] > thresh else "black")
         else:
             plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     fontsize=26,
-                     weight='bold',
+                     horizontalalignment="center",fontsize=26, weight='bold',
                      color="white" if cm[i, j] > thresh else "black")
 
 
     plt.tight_layout()
-    plt.xlabel('Predicted Class\nAccuracy={:0.3f}; Misclass={:0.3f}'.format(accuracy, misclass),fontsize=20)
+    plt.ylabel('Actual Class',fontsize=20)
+    plt.xlabel('Predicted label\nAccuracy={:0.3f}; Misclass={:0.3f}'.format(accuracy, misclass),fontsize=20)
     plt.show()
-    
-plot_confusion_matrix(cm = np.array([[9309,788],
-                                    [951,13386]]), 
+    plt.savefig('Confusion_Matrix.png', bbox_inches='tight', dpi=600)
+
+plot_confusion_matrix(cm = np.array([[ 1827,  5],
+                                    [  4,  1812]]), 
                       normalize    = False,
-                      target_names = ['0', '1'],
-                      title        = "<title>")
+                      target_names = ['Normal', 'Attack'],
+                      title        = "Confusion Matrix")
